d61a2a975b2c10aa08fa883d28ff3f07c904f1a1
Evan Hazlett <ejhazlett@gmail.com>
2017-07-18 18:53:22 -0400

    add helper package for external plugins
    Signed-off-by: Evan Hazlett <ejhazlett@gmail.com>

--- plugin/config.go
+++ plugin/config.go
@@ -1,15 +1,54 @@
 package plugin
 
 import (
+	"encoding/json"
+	"os"
+
 	"github.com/ehazlett/interlock/api/services/configuration"
+	"github.com/ehazlett/interlock/api/types"
 	"golang.org/x/net/context"
 )
 
-func (p *Plugin) Config(ctx context.Context) (*configuration.Config, error) {
+const (
+	configPath = "/etc/interlock/config"
+)
+
+// LoadServiceConfig loads the service configuration from the well known location
+func LoadServiceConfig() (*types.ServiceConfig, error) {
+	if _, err := os.Stat(configPath); err != nil {
+		if os.IsNotExist(err) {
+			return nil, nil
+		}
+
+		return nil, err
+	}
+
+	f, err := os.Open(configPath)
+	if err != nil {
+		return nil, err
+	}
+
+	cfg := &types.ServiceConfig{}
+	if err := json.NewDecoder(f).Decode(&cfg); err != nil {
+		return nil, err
+	}
+
+	return cfg, nil
+}
+
+func (p *Plugin) Config(ctx context.Context) (*configuration.ConfigResponse, error) {
 	config, err := p.client.Config(ctx, &configuration.ConfigRequest{})
 	if err != nil {
 		return nil, err
 	}
 
-	return config.Config, nil
+	return config, nil
+}
+
+func (p *Plugin) Endpoint() string {
+	if p.serviceConfig == nil {
+		return ""
+	}
+
+	return p.serviceConfig.Endpoint
 }
--- plugin/plugin.go
+++ plugin/plugin.go
@@ -2,22 +2,36 @@ package plugin
 
 import (
 	"github.com/ehazlett/interlock/api/services/configuration"
+	"github.com/ehazlett/interlock/api/types"
 	"google.golang.org/grpc"
 )
 
 type Plugin struct {
-	client configuration.ConfigurationClient
+	client        configuration.ConfigurationClient
+	serviceConfig *types.ServiceConfig
 }
 
 func NewPlugin(addr string) (*Plugin, error) {
+	p := &Plugin{}
+	if addr == "" {
+		cfg, err := LoadServiceConfig()
+		if err != nil {
+			return nil, err
+		}
+
+		p.serviceConfig = cfg
+
+		if cfg != nil {
+			addr = cfg.Endpoint
+		}
+	}
+
 	conn, err := grpc.Dial(addr, grpc.WithInsecure())
 	if err != nil {
 		return nil, err
 	}
 
-	client := configuration.NewConfigurationClient(conn)
+	p.client = configuration.NewConfigurationClient(conn)
 
-	return &Plugin{
-		client: client,
-	}, nil
+	return p, nil
 }
